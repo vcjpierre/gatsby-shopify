1480ebdd0d61f11fdb9530650825c43244725913
Jean Pierre Velez <vcjpierre@gmail.com>
2021-06-02 01:38:04 -0400

    Create image gallery component

--- src/templates/ProductTemplate/index.js
+++ src/templates/ProductTemplate/index.js
@@ -2,25 +2,37 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import { Layout } from 'components';
 import { Grid } from './styles';
+import { ImageGallery } from '../../components/ImageGallery';
 
 export const query = graphql`
   query ProductQuery($shopifyId: String) {
     shopifyProduct(shopifyId: { eq: $shopifyId }) {
       title
+      description
+      images {
+        id
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 300) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
     }
   }
 `;
 
 export default function ProductTemplate(props) {
-  console.log(props);
   return (
     <Layout>
       <Grid>
         <div>
           <h1>{props.data.shopifyProduct.title}</h1>
+          <p>{props.data.shopifyProduct.description}</p>
         </div>
         <div>
-          
+          <ImageGallery images={props.data.shopifyProduct.images} />
         </div>
       </Grid>
     </Layout>
