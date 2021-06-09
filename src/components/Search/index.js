5395c325d64c7bffc181be32361fbd2c8bedbdec
Jean Pierre Velez <vcjpierre@gmail.com>
2021-06-09 00:00:09 -0400

    Creating the search component

---
+++ src/components/Search/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Input } from '../Input';
+import { Button } from '../Button';
+import { FaSearch } from 'react-icons/fa';
+import { SearchForm } from './styles';
+import { useLocation } from '@reach/router';
+import { navigate } from 'gatsby';
+import queryString from 'query-string';
+
+export function Search() {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const { search } = useLocation();
+  const c = queryString.parse(search)?.c || '';
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    if (c) {
+      navigate(
+        `/all-products?s=${encodeURIComponent(
+          searchTerm
+        )}&c=${encodeURIComponent(c)}`
+      );
+    } else {
+      navigate(`/all-products?s=${encodeURIComponent(searchTerm)}`);
+    }
+  };
+
+  return (
+    <SearchForm onSubmit={handleSubmit}>
+      <Input
+        value={searchTerm}
+        onChange={e => setSearchTerm(e.currentTarget.value)}
+        placeholder="Search"
+      />
+      <Button>
+        <FaSearch />
+      </Button>
+    </SearchForm>
+  );
+}
